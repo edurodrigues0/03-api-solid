c5f228a63c280a853ccddfcf1717331cc78dcc6e
edurodrigues0 <edusacramento9@gmail.com>
2023-10-11 20:06:22 -0300

    Caso de Uso e Design Patterns - Interface do repositório

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,9 +1,8 @@
 import { prisma } from "@/lib/prisma"
 import { z } from "zod"
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { hash } from 'bcryptjs'
 import { RegisterUseCase } from "@/use-cases/register"
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository"
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
