0d7d572d4badd53689a6017263eda9703ddabe4a
edurodrigues0 <edusacramento9@gmail.com>
2023-10-16 17:49:48 -0300

    Implementando casos de uso - Caso de uso de check-in

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,34 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "@/repositories/check-ins-repository";
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(
+    private checkInsRepostiroy: CheckInsRepository
+  ) {}
+
+  async execute({ 
+    userId,
+    gymId
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepostiroy.create({
+      gym_id: gymId,
+      user_id: userId
+    })
+
+    return {
+      checkIn
+    }
+  }
+}
