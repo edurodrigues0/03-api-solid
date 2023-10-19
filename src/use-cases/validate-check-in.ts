428b878a107b136fca00a9b5162d747a707f357c
edurodrigues0 <edusacramento9@gmail.com>
2023-10-19 20:23:36 -0300

    Implementando casos de uso - Validando horário do check-in

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -5,6 +5,8 @@ import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 import { MaxNumberOfCheckInError } from "./errors/max-number-of-check-ins-error";
 import { MaxDistanceError } from "./errors/max-distance-error";
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation";
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -26,6 +28,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError()
     }
 
     checkIn.validated_at = new Date()
