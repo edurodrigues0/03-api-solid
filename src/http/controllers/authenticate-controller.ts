34673475abafa2f9a95c97af6ae8e072cccedeec
edurodrigues0 <edusacramento9@gmail.com>
2023-10-24 21:09:13 -0300

    Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -14,19 +14,27 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password
     })
+
+    const token = await reply.jwtSign(
+      {}, 
+      {
+        sign: {
+        sub: user.id,
+        },
+      }
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
-      reply.status(400).send({
-        message: err.message
-      })
+      return reply.status(400).send({ message: err.message })
     }
-
     throw err
   }
-
-  return reply.status(200).send()
 }
