7d2b82e1b079bc96f64b523cf7c97c91e5a95055
edurodrigues0 <edusacramento9@gmail.com>
2023-10-26 11:42:28 -0300

    Controllers & Testes E2E - Outros controllers da academia

--- src/http/controllers/gyms/create-controller.ts
+++ src/http/controllers/gyms/create-controller.ts
@@ -1,7 +1,5 @@
 import { z } from "zod"
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error"
-import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case"
 import { makeCreateGymUseCase } from "@/use-cases/factories/make-create-gym-use-case"
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
