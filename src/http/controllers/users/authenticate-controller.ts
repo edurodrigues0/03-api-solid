d98a9ac0f784b970c15e5247955af150c9820747
edurodrigues0 <edusacramento9@gmail.com>
2023-10-27 19:46:01 -0300

    Refresh Token & RBAC - Estratégia de refresh de token

--- src/http/controllers/users/authenticate-controller.ts
+++ src/http/controllers/users/authenticate-controller.ts
@@ -23,13 +23,31 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
       {}, 
       {
         sign: {
-        sub: user.id,
+          sub: user.id,
         },
       }
     )
 
-    return reply.status(200).send({
-      token,
+    const refreshToken = await reply.jwtSign(
+      {}, 
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d'
+        },
+      }
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
     })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
